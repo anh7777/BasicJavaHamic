27dfdc264a71d6d6edcd43f1703f6dd388cef935
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-15 21:12:29 +0700

    Update AddExercise2

---
+++ Exercise2/AddExercise2/Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập n: ");
+        int n = scanner.nextInt();
+        System.out.println("P(" + n + ") = " + factorial(n));
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
