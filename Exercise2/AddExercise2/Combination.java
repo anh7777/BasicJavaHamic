e7c0c00edc10d529d1e841c7092af344c92b833b
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-16 00:04:46 +0700

    Update Combination.java

--- Exercise2/AddExercise2/Combination.java
+++ Exercise2/AddExercise2/Combination.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Combination {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập n: ");
+        System.out.print("Enter n: ");
         int n = scanner.nextInt();
-        System.out.print("Nhập k: ");
+        System.out.print("Enter k: ");
         int k = scanner.nextInt();
         System.out.println("C(" + n + ", " + k + ") = " + combination(n, k));
     }
