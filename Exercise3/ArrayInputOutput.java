bf12e393ec9f2dbb3849b45b16af1d1e6e3a3989
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-15 21:32:18 +0700

    Update Exercise3

---
+++ Exercise3/ArrayInputOutput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ArrayInputOutput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số lượng phần tử của mảng: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Nhập các phần tử của mảng:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Mảng các số nguyên:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
