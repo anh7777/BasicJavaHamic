23b2d305b76056c93c4b503176e3e23afa58915f
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-16 00:11:44 +0700

    Update Permutation.java

--- Exercise2/AddExercise2/Permutation.java
+++ Exercise2/AddExercise2/Permutation.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Permutation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập k: ");
+        System.out.print("Enter k: ");
         int k = scanner.nextInt();
-        System.out.print("Nhập n: ");
+        System.out.print("Enter n: ");
         int n = scanner.nextInt();
         System.out.println("A(" + k + ", " + n + ") = " + permutation(k, n));
     }
