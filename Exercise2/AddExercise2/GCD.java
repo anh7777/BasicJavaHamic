7c823279289687fdfe2044105bc003216a61a1cc
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-16 00:08:28 +0700

    Update GCD.java

--- Exercise2/AddExercise2/GCD.java
+++ Exercise2/AddExercise2/GCD.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class GCD {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập số a: ");
+        System.out.print("Enter a: ");
         int a = scanner.nextInt();
-        System.out.print("Nhập số b: ");
+        System.out.print("Enter b: ");
         int b = scanner.nextInt();
-        System.out.println("Ước chung lớn nhất của " + a + " và " + b + " là: " + gcd(a, b));
+        System.out.println("GCD of  " + a + " and " + b + " is: " + gcd(a, b));
     }
 
     public static int gcd(int a, int b) {
