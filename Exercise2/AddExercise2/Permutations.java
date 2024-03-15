93c4ef2ad2269aa4537bdbb5677d1f66219884b1
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-15 21:17:58 +0700

    Update AddExercise2

---
+++ Exercise2/AddExercise2/Permutations.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Permutations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số n: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+        System.out.println("Các hoán vị của " + n + " số đầu tiên là:");
+        generatePermutations(arr, 0);
+    }
+
+    public static void generatePermutations(int[] arr, int index) {
+        if (index == arr.length - 1) {
+            printArray(arr);
+        } else {
+            for (int i = index; i < arr.length; i++) {
+                swap(arr, index, i);
+                generatePermutations(arr, index + 1);
+                swap(arr, index, i);
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
