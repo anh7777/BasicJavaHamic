39e9d6f1bfe08f444b124aa4323587611b885371
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-16 00:06:49 +0700

    Update DecimalToOctal.java

--- Exercise2/AddExercise2/DecimalToOctal.java
+++ Exercise2/AddExercise2/DecimalToOctal.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class DecimalToOctal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập số nguyên dương n: ");
+        System.out.print("Enter positive number n: ");
         int n = scanner.nextInt();
-        System.out.print("Số " + n + " trong hệ bát phân là: ");
         convertToOctal(n);
     }
 
