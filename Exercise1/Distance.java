e4d9eb295da6852c1c51d95f1168070a1a4b0a87
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-16 00:17:58 +0700

    Update Distance.java

--- Exercise1/Distance.java
+++ Exercise1/Distance.java
@@ -4,18 +4,18 @@ public class Distance {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Nhập tọa độ của điểm A (x, y, z): ");
+        System.out.print("Enter A (x, y, z): ");
         double x1 = scanner.nextDouble();
         double y1 = scanner.nextDouble();
         double z1 = scanner.nextDouble();
 
-        System.out.print("Nhập tọa độ của điểm B (a, b, c): ");
+        System.out.print("Enter B (a, b, c): ");
         double x2 = scanner.nextDouble();
         double y2 = scanner.nextDouble();
         double z2 = scanner.nextDouble();
 
         double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) + Math.pow(z2 - z1, 2));
 
-        System.out.println("Khoảng cách giữa hai điểm là: " + distance);
+        System.out.println("Distance AB: " + distance);
     }
 }
