a835e66241a8c7a84729f8eb3274f6f0206e4e1c
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-16 00:14:53 +0700

    Update SeriesNthNumber.java

--- Exercise2/AddExercise2/SeriesNthNumber.java
+++ Exercise2/AddExercise2/SeriesNthNumber.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class SeriesNthNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập số n: ");
+        System.out.print("Enter n: ");
         int n = scanner.nextInt();
-        System.out.println("Số thứ " + n + " của dãy là: " + nthNumber(n));
+        System.out.println("the number " + n + " of array is: " + nthNumber(n));
     }
 
     public static double nthNumber(int n) {
