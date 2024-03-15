d7910f6c865850becfe3f0ebd93d2cac756658a6
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-15 21:04:43 +0700

    Update Exercise31.java

--- Exercise2/Exercise31.java
+++ Exercise2/Exercise31.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Exercise31 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
