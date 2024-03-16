efd904528c3e2095d8f87a8ebd9405685179e272
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-16 09:30:34 +0700

    Update NthSmallestInteger.java

--- Exercise3/NthSmallestInteger.java
+++ Exercise3/NthSmallestInteger.java
@@ -2,22 +2,22 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class NthSmallestInteger {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập số lượng phần tử của mảng: ");
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        System.out.println("Nhập các phần tử của mảng:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
-        System.out.print("Nhập số n: ");
-        int k = scanner.nextInt();
-        Arrays.sort(arr);
-        if (k <= arr.length) {
-            System.out.println("Số nhỏ thứ " + k + " trong mảng là: " + arr[k - 1]);
-        } else {
-            System.out.println("Không có số nhỏ thứ " + k + " trong mảng.");
-        }
-    }
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Enter number of array elements: ");
+         int n = scanner.nextInt();
+         int[] arr = new int[n];
+         System.out.println("Enter array elements:");
+         for (int i = 0; i < n; i++) {
+             arr[i] = scanner.nextInt();
+         }
+         System.out.print("Enter number n: ");
+         int k = scanner.nextInt();
+         Arrays.sort(arr);
+         if (k <= arr.length) {
+             System.out.println("The smallest number " + k + " in the array is: " + arr[k - 1]);
+         } else {
+             System.out.println("There is no small number " + k + " in the array.");
+         }
+     }
 }
