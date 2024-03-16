716014dedb8df3a902093a7c68da910afa34261e
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-16 09:29:25 +0700

    Update IsStrictlyIncreasing.java

--- Exercise3/IsStrictlyIncreasing.java
+++ Exercise3/IsStrictlyIncreasing.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
 public class IsStrictlyIncreasing {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập số lượng phần tử của mảng: ");
-        int n = scanner.nextInt();
-        double[] arr = new double[n];
-        System.out.println("Nhập các phần tử của mảng:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextDouble();
-        }
-        System.out.println("Mảng " + (isStrictlyIncreasing(arr) ? "là" : "không là") + " dãy tăng thực sự.");
-    }
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Enter number of array elements: ");
+         int n = scanner.nextInt();
+         double[] arr = new double[n];
+         System.out.println("Enter array elements:");
+         for (int i = 0; i < n; i++) {
+             arr[i] = scanner.nextDouble();
+         }
+         System.out.println("Array " + (isStrictlyIncreasing(arr) ? "is" : "is not") + " actually increasing sequence.");
+     }
 
-    public static boolean isStrictlyIncreasing(double[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] <= arr[i - 1]) {
-                return false;
-            }
-        }
-        return true;
-    }
+     public static boolean isStrictlyIncreasing(double[] arr) {
+         for (int i = 1; i < arr.length; i++) {
+             if (arr[i] <= arr[i - 1]) {
+                 return false;
+             }
+         }
+         return true;
+     }
 }
