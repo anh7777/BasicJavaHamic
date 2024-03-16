74f5a0fa234ae3de06c460f004ce96b1aeb962cb
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-16 09:30:06 +0700

    Update MaxRealNumber.java

--- Exercise3/MaxRealNumber.java
+++ Exercise3/MaxRealNumber.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 
 public class MaxRealNumber {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập số lượng phần tử của mảng: ");
-        int n = scanner.nextInt();
-        double[] arr = new double[n];
-        System.out.println("Nhập các phần tử của mảng:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextDouble();
-        }
-        System.out.println("Số lớn nhất trong mảng là: " + findMax(arr));
-    }
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Enter number of array elements: ");
+         int n = scanner.nextInt();
+         double[] arr = new double[n];
+         System.out.println("Enter array elements:");
+         for (int i = 0; i < n; i++) {
+             arr[i] = scanner.nextDouble();
+         }
+         System.out.println("The largest number in the array is: " + findMax(arr));
+     }
 
-    public static double findMax(double[] arr) {
-        double max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-        return max;
-    }
+     public static double findMax(double[] arr) {
+         double max = arr[0];
+         for (int i = 1; i < arr.length; i++) {
+             if (arr[i] > max) {
+                 max = arr[i];
+             }
+         }
+         return max;
+     }
 }
