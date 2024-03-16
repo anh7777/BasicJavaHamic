1bafc24e416ef82cb54bfc1e1353a0f4eb9f4364
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-16 09:32:45 +0700

    Update SumOfRealNumbers.java

--- Exercise3/SumOfRealNumbers.java
+++ Exercise3/SumOfRealNumbers.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 
 public class SumOfRealNumbers {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập số lượng phần tử của mảng: ");
-        int n = scanner.nextInt();
-        double[] arr = new double[n];
-        System.out.println("Nhập các phần tử của mảng:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextDouble();
-        }
-        System.out.println("Tổng các phần tử trong mảng là: " + findSum(arr));
-    }
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Enter number of array elements: ");
+         int n = scanner.nextInt();
+         double[] arr = new double[n];
+         System.out.println("Enter array elements:");
+         for (int i = 0; i < n; i++) {
+             arr[i] = scanner.nextDouble();
+         }
+         System.out.println("The sum of the elements in the array is: " + findSum(arr));
+     }
 
-    public static double findSum(double[] arr) {
-        double sum = 0;
-        for (double num : arr) {
-            sum += num;
-        }
-        return sum;
-    }
+     public static double findSum(double[] arr) {
+         double sum = 0;
+         for (double num : arr) {
+             sum += num;
+         }
+         return sum;
+     }
 }
