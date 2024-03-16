e7f2e6ef74ac04ba3bc75922860fa00da28329bb
anh7777 <151049962+anh7777@users.noreply.github.com>
2024-03-16 09:31:01 +0700

    Update SortDescendingOrder.java

--- Exercise3/SortDescendingOrder.java
+++ Exercise3/SortDescendingOrder.java
@@ -2,20 +2,20 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class SortDescendingOrder {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập số lượng phần tử của mảng: ");
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        System.out.println("Nhập các phần tử của mảng:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
-        Arrays.sort(arr);
-        System.out.println("Mảng sau khi sắp xếp giảm dần:");
-        for (int i = n - 1; i >= 0; i--) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Enter number of array elements: ");
+         int n = scanner.nextInt();
+         int[] arr = new int[n];
+         System.out.println("Enter array elements:");
+         for (int i = 0; i < n; i++) {
+             arr[i] = scanner.nextInt();
+         }
+         Arrays.sort(arr);
+         System.out.println("Array after sorting in descending order:");
+         for (int i = n - 1; i >= 0; i--) {
+             System.out.print(arr[i] + " ");
+         }
+         System.out.println();
+     }
 }
